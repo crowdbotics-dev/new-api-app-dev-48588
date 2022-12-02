98d37708634aee69df5b12c62a402baca546bf97
Ravinder sales <ravinder.y@crowdbotics.com>
2022-12-02 16:42:45 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,9 +1,10 @@
+import { Text } from "react-native";
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
 const Untitled1 = () => {
   return <View style={_styles.HLGQLIvJ}>
-      <View style={_styles.akNnkhyz}></View></View>;
+      <View style={_styles.akNnkhyz}></View><Text style={_styles.ESDxVgGX}>Lorem ipsum…</Text></View>;
 };
 
 export default Untitled1;
@@ -28,5 +29,12 @@ const _styles = StyleSheet.create({
     backgroundColor: "#E4E4E4",
     borderRadius: 0,
     color: "#777777"
+  },
+  ESDxVgGX: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
   }
 });
