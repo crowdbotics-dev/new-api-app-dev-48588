c53c8d7b6cf83b728934f8d343d993d90dd901b3
Ravinder sales <ravinder.y@crowdbotics.com>
2022-12-02 16:43:05 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,10 +1,9 @@
-import { Text } from "react-native";
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
 const Untitled1 = () => {
   return <View style={_styles.HLGQLIvJ}>
-      <View style={_styles.akNnkhyz}></View><Text style={_styles.ESDxVgGX}>Lorem ipsum…</Text></View>;
+      </View>;
 };
 
 export default Untitled1;
